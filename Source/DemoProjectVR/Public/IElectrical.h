0b59808737f79eba68010b29325b0d8d1c3e11cd
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-06-30 12:23:40 +0900

    ロッカー制作開始
    クラス作成のみ

---
+++ Source/DemoProjectVR/Public/IElectrical.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+// 電源（インターフェース利用）
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GimmickInterface.h"
+
+#include "IElectrical.generated.h"
+
+UCLASS()
+class DEMOPROJECTVR_API AIElectrical : public AActor , public IGimmickInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AIElectrical();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void Action() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/DemoProjectVR/Public/ILockerDoor.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+// ロッカーのドア（インターフェース利用）
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GimmickInterface.h"
+
+#include "ILockerDoor.generated.h"
+
+UCLASS()
+class DEMOPROJECTVR_API AILockerDoor : public AActor , public IGimmickInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AILockerDoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void Action() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
