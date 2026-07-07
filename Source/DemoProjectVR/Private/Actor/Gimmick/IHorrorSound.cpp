774dba21f060ccc6943b550564c862b2eecfd7cb
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-07-07 12:26:16 +0900

    ホラーサウンドギミッククラス実装
    心電図とナースコールの基盤ができました。
    あとデバッグ機能でC++からprintstringが簡単にできるマクロを追加しました。
    |-DebugHelper.h

---
+++ Source/DemoProjectVR/Private/Actor/Gimmick/IHorrorSound.cpp
@@ -0,0 +1,125 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actor/Gimmick/IHorrorSound.h"
+
+// Sets default values
+AIHorrorSound::AIHorrorSound()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIHorrorSound::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	bool isNull = !audioComponent || !sound;
+	if (isNull) 
+	{
+		DEBUG_PRINT("%s : AudioComponent または Sound が設定されていません。", *GetName());
+	}
+	else
+	{
+		// サウンドデータをコンポーネントに渡す
+		audioComponent->SetSound(sound);
+		
+
+		// デリゲート登録
+		bool isECG = gimmickType == EHorrorSoundGimmickType::ElectroCardiogram;
+		if (isECG)
+		{
+			audioComponent->OnAudioFinished.AddDynamic(
+				this,
+				&AIHorrorSound::GimmickPlay
+			);
+		}
+	}
+
+	if (!SetCamera())
+	{
+		DEBUG_PRINT("%s : Player または CameraComponent が見つかりません。", *GetName());
+	}
+
+	isPlayLoop = true;
+}
+
+// Called every frame
+void AIHorrorSound::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// サウンド再生チェック	
+	bool isNull = !audioComponent || !sound || !camera;
+	if (isNull)
+	{
+		return;
+	}
+
+	FVector camPos = camera->GetComponentLocation();
+	FVector thisPos = GetActorLocation();
+	float distance = FVector::Distance(camPos, thisPos);
+	
+	// サウンドの再生速度(ピッチ)を調整
+	bool isECG = gimmickType == EHorrorSoundGimmickType::ElectroCardiogram;
+	if (isECG)
+	{
+		float percent = distance / detectionRadius;
+		float newTempo = tempo + ( 1.0f - percent );
+		audioComponent->SetPitchMultiplier(newTempo);
+	}
+
+	bool isActive = distance <= detectionRadius;
+	isActive &= !IsPlayingCheck();
+	isActive &= !hasControll;
+	if (isActive)
+	{
+		GimmickPlay();
+	}
+}
+
+// 停止
+void AIHorrorSound::Action()
+{
+	isPlayLoop = false;
+	audioComponent->Stop();
+}
+
+// アクティブチェック
+bool AIHorrorSound::IsPlayingCheck()
+{
+	bool rtv = audioComponent->IsPlaying();
+	return rtv;
+}
+
+// ギミック再生
+void AIHorrorSound::GimmickPlay()
+{
+	if(!IsPlayingCheck() && isPlayLoop)
+	{
+		audioComponent->Play();
+	}
+}
+
+// カメラコンポーネントの取得
+bool AIHorrorSound::SetCamera()
+{
+	APawn* playerPawn =
+    UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+	if (!playerPawn)
+	{
+		return false;
+	}
+
+	camera = playerPawn->FindComponentByClass<UCameraComponent>();
+
+	if (!camera)
+	{
+		return false;
+	}
+
+	return true;
+}
+
---
+++ Source/DemoProjectVR/Public/DebugHelper.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#if !UE_BUILD_SHIPPING
+
+#define DEBUG_PRINT(Format, ...) \
+{ \
+    FString Msg = FString::Printf(TEXT(Format), ##__VA_ARGS__); \
+    if (GEngine) \
+    { \
+        GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Orange, Msg); \
+    } \
+    UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg); \
+}
+
+#else
+
+#define DEBUG_PRINT(Format, ...)
+
+#endif
