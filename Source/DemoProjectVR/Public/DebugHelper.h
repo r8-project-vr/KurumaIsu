09beca6dabbe8e5bc01a9857bc7329813ef0391e
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-07-15 19:06:30 +0900

    デバッグ用マクロ修正

--- Source/DemoProjectVR/Public/DebugHelper.h
+++ Source/DemoProjectVR/Public/DebugHelper.h
@@ -2,6 +2,7 @@
 
 #if !UE_BUILD_SHIPPING
 
+// 文字列を画面とログに表示するマクロ（文字列はprintfフォーマットで指定）
 #define DEBUG_PRINT(Format, ...) \
 { \
     FString Msg = FString::Printf(TEXT(Format), ##__VA_ARGS__); \
@@ -12,6 +13,7 @@
     UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg); \
 }
 
+// 任意の列挙型のデータ名を文字列で返すマクロ
 #define ENUM_STR(Value) \
     *StaticEnum<std::decay_t<decltype(Value)>>()->GetNameStringByValue((int64)Value)
 
