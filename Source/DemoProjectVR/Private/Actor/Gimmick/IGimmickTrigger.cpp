78c052fe4ac8e72d67a02723cf6009352dcce736
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-07-24 14:34:42 +0900

    ギミック起動用BP実装

--- Source/DemoProjectVR/Private/Actor/Gimmick/IGimmickTrigger.cpp
+++ Source/DemoProjectVR/Private/Actor/Gimmick/IGimmickTrigger.cpp
@@ -8,14 +8,22 @@ AIGimmickTrigger::AIGimmickTrigger()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+    TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+    RootComponent = TriggerBox;
 
+    TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    TriggerBox->SetCollisionResponseToAllChannels(ECR_Ignore);
+    TriggerBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
 void AIGimmickTrigger::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// イベント登録
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AIGimmickTrigger::OnBeginOverlap);
 }
 
 // Called every frame
@@ -35,5 +43,17 @@ void AIGimmickTrigger::Action()
 			gimmick->Action();
 		}
 	}
+}
+
+// トリガーイベント
+void AIGimmickTrigger::OnBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult)
+{
+	Action();
 }
 
--- Source/DemoProjectVR/Public/Actor/Gimmick/IGimmickTrigger.h
+++ Source/DemoProjectVR/Public/Actor/Gimmick/IGimmickTrigger.h
@@ -5,6 +5,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
 #include "GimmickInterface.h"
 
 #include "IGimmickTrigger.generated.h"
@@ -24,6 +25,15 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+    void OnBeginOverlap(
+        UPrimitiveComponent* OverlappedComponent,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult);
+
 private:
 	UFUNCTION(BlueprintCallable)
 	void Action() override;
@@ -35,4 +45,8 @@ public:
 	// このアクターが実行するギミックリスト
 	UPROPERTY(EditAnywhere, Category = "Setting")
 	TArray<TScriptInterface<IGimmickInterface>> gimmickes;
+
+	// 起動させるトリガー
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	TObjectPtr<UBoxComponent> TriggerBox;
 };
