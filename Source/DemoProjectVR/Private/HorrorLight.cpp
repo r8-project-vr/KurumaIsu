3ffd24f0bd1c62657d8ac335dfbb78293dcc39fe
sqrqmi <25ci0333@jec.ac.jp>
2026-09-05 15:42:19 +0900

    ライト制作

---
+++ Source/DemoProjectVR/Private/HorrorLight.cpp
@@ -0,0 +1,44 @@
+﻿ // Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HorrorLight.h"
+
+// Sets default values
+AHorrorLight::AHorrorLight()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RectLight == CreateDefaultSubobject<URectLightComponent>(TEXT("RectLight"));
+	RootComponent = RectLight;
+}
+
+// Called when the game starts or when spawned
+void AHorrorLight::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHorrorLight::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	bool isOutage = outageTime <= 0.0f;
+	if (isOutage)
+	{
+
+	}
+	else
+	{
+		outageTime -= DeltaTime;
+	}
+}
+
+void AHorrorLight::LightOutage()
+{
+	RectLight->Intensity = 0.0f;
+	outageTime = FMath::FRandRange(intervalMin, intervalMax);
+}
+
---
+++ Source/DemoProjectVR/Public/HorrorLight.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/RectLightComponent.h"
+
+#include "HorrorLight.generated.h"
+
+UCLASS()
+class DEMOPROJECTVR_API AHorrorLight : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHorrorLight();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	URectLightComponent* RectLight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setting")
+	UCurveFloat* moveCurve;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	float intensityMax = 2400.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	float restorationTime = 3.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	float intervalMin = 1.5f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	float intervalMax = 7.0f;
+private:
+	float outageTime = 0.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UFUNCTION()
+	void LightOutage();
+};
