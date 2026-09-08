56f2953ea4da08098dd6619dcf1e432fdefed982
sqrqmi <286570095+jec-25ci0333@users.noreply.github.com>
2026-09-08 16:59:00 +0900

    ライト追加

---
+++ Source/DemoProjectVR/Public/HorrorSpotLight.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SpotLightComponent.h"
+
+#include "HorrorSpotLight.generated.h"
+
+UCLASS()
+class DEMOPROJECTVR_API AHorrorSpotLight : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHorrorSpotLight();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Setting")
+	USpotLightComponent* spotLight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setting")
+	UCurveFloat* moveCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Setting")
+	float intensityMax = 2400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Setting")
+	float loopTime = 3.0f;
+
+private:
+	float elapsedTime = 0.0f;
+};
